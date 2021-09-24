a1000e1dbfb173e0ad0e199448e9588c84713492
Christofer cardone <chriscardone69@gmail.com>
2021-09-24 23:52:01 +0200

    frontend product page improved with grid

--- frontend/src/pages/ProductDetail.js
+++ frontend/src/pages/ProductDetail.js
@@ -1,31 +1,42 @@
 import React from "react";
 import ProductRating from "../components/Product/ProductRating";
 
+import { useParams } from "react-router";
+
+import data from "../data";
+
 const ProductDetail = () => {
+  const params = useParams();
+  console.log(params);
+  const ProductId = params.productId;
+
+  const product = data.products.filter((product)=>product._id === ProductId);
+
+  console.log(product);
+
   return (
     <section className="grid-detail">
       <figure className="product-detail-image-section">
         <img src="https://picsum.photos/200/300?a=2" alt="Image" />
       </figure>
       <div className="product-detail-details-section">
-          <h2>Product Title</h2>
-          <ProductRating />
-          <p>Price : $ 100</p>
-          <p>Description: High quality product</p>
+        <h2>Product Title</h2>
+        <ProductRating />
+        <p>Price : $ 100</p>
+        <p>Description: High quality product</p>
       </div>
       <div className="product-detail-add-to-chart">
-          <div className="row">
-              <p>Price</p>
-              <p>$ 100</p>
-          </div>
-          <div className="row">
-              <p>Status</p>
-              <p>in Stock</p>
-          </div>
-        <div>
-            <button>Add to Cart</button>
+        <div className="row">
+          <p>Price</p>
+          <p>$ 100</p>
         </div>
-
+        <div className="row">
+          <p>Status</p>
+          <p>in Stock</p>
+        </div>
+        <div>
+          <button>Add to Cart</button>
+        </div>
       </div>
     </section>
   );
