d5afeba39259833ff42f0a0915eb25cab30d6839
Christofer cardone <chriscardone69@gmail.com>
2021-09-24 16:38:28 +0200

    products dynamically fetched from a dummy data

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import data from "./data";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="grid-container">
+        <header className="row">
+          <div>
+            <a className="brand" href="/">
+              Space
+            </a>
+          </div>
+          <div>
+            <a href="/cart">Cart</a>
+            <a href="/signin">Sign In</a>
+          </div>
+        </header>
+        <main>
+          <section className="grid-products">
+            {data.products.map((product) => (
+              <div key={product._id} className="product-wrapper">
+                <figure>
+                  <img src={product.image} alt={product.name} />
+                  <div className="details-wrapper">
+                    <h2>{product.name}</h2>
+                    <ul className="row">
+                      <li>
+                        <i className="fas fa-star-half-alt"></i>
+                      </li>
+                      <li>
+                        <i className="fas fa-star"></i>
+                      </li>
+                      <li>
+                        <i className="fas fa-star"></i>
+                      </li>
+                      <li>
+                        <i className="fas fa-star"></i>
+                      </li>
+                      <li>
+                        <i className="far fa-star"></i>
+                      </li>
+                    </ul>
+                    <p>{product.price} € </p>
+                  </div>
+                </figure>
+              </div>
+            ))}
+          </section>
+        </main>
+        <footer className="row center">All right reserved</footer>
+      </div>
     </div>
   );
 }
---
+++ frontend/src/data.js
@@ -0,0 +1,50 @@
+const data = {
+  products: [
+    {
+      _id: 1,
+      name: "Nike",
+      category: "shirts",
+      image: "https://picsum.photos/200/300?a=2",
+      price: 200,
+      brand: "Nikeike",
+      rating: 4.5,
+      numReviewes: 10,
+      description: "high quality ",
+    },
+    {
+      _id: 2,
+      name: "Nike",
+      category: "shirts",
+      image: "https://picsum.photos/200/300?b=2",
+      price: 200,
+      brand: "",
+      rating: 4.5,
+      numReviewes: 10,
+      description: "high quality ",
+    },
+    {
+      _id: 3,
+      name: "Nike",
+      category: "shirts",
+      image: "https://picsum.photos/200/300?c=2",
+      price: 200,
+      brand: "",
+      rating: 4.5,
+      numReviewes: 10,
+      description: "high quality ",
+    },
+    {
+      _id: 4,
+      name: "Nike",
+      category: "shirts",
+      image: "https://picsum.photos/200/300?d=2",
+      price: 200,
+      brand: "",
+      rating: 4.5,
+      numReviewes: 10,
+      description: "high quality ",
+    },
+  ],
+};
+
+export default data;
