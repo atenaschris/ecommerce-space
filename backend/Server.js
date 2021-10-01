37d85b55091015618717b22f96c02ffce714eb62
Christofer cardone <chriscardone69@gmail.com>
2021-10-01 12:05:55 +0200

    users data created with migration, model and userRouter, connection to the database and error handling middleware as well

--- backend/Server.js
+++ backend/Server.js
@@ -1,7 +1,11 @@
 import express from "express";
+import mongoose from "mongoose";
 import data from "./data.js";
+import userRouter from "./routers/userRouter.js";
 
 const app = express();
+
+mongoose.connect(process.env.MONGODB_URL || "mongodb://localhost/space");
 
 app.get("/", (req, res) => {
   res.send("Server is ready");
@@ -11,7 +15,7 @@ app.get("/api/products", (req, res) => {
 });
 
 app.get("/api/products/:id", (req, res) => {
-    console.log( typeof(req.params.id));
+  console.log(typeof req.params.id);
   const filteredProduct = data.products.find((el) => el._id === +req.params.id);
 
   console.log(filteredProduct);
@@ -20,6 +24,12 @@ app.get("/api/products/:id", (req, res) => {
   } else {
     res.status(404).send({ message: "Product not found!!" });
   }
+});
+
+app.use("/api/users", userRouter);
+
+app.use((err, req, res, next) => {
+  res.status(500).send({ message: err.message });
 });
 
 const port = process.env.PORT || 5000;
